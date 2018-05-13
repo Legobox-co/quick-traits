4c47a7bc075f4e3a9a36613dc590267524fbccd4
Chibuike Emmauel Osita <mozart.osita@gmail.com>
2018-05-13 22:35:23 +0100

    updates

---
+++ src/Events/RegisteredModel.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Legobox\QuickTraits\Events;
+
+use Auth;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Database\Eloquent\Model;
+
+class RegisteredModel implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+
+	 public $model;
+
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('registered.model.'.Auth::user()->id);
+	}
+	
+	public function broadcastWith(){
+		return [
+			'message' => 'new '.return_class($this->model)->getShortName().' has been created',
+			'data' => $this->model
+		];
+	}
+}
